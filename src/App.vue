1d6b7cc3835d3ea84587c1b2250e0c2ecabdec07
XP_Lin <3972888185@qq.com>
2025-08-25 19:07:15 +0800

    Add files via upload

---
+++ src/App.vue
@@ -0,0 +1,37 @@
+<template>
+
+  <!-- 主体区域 -->
+  <section id="app">
+    <DoHeader></DoHeader>
+    <DoMain :list="list"></DoMain>
+    <DoFooter></DoFooter>
+  </section>
+</template>
+
+<script>
+import DoHeader from './components/DoHeader.vue';
+import DoMain from './components/DoMain.vue';
+import DoFooter from './components/DoFooter.vue';
+export default {
+  components:{
+    DoHeader,
+    DoMain,
+    DoFooter
+  },
+
+  data () {
+    return {
+      // 准备数据
+      list:[
+        {id:1,name:'吃好👌！'},
+        {id:2,name:'喝好👌！'},
+        {id:3,name:'睡好👌！'}
+      ]
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
---
+++ src/components/DoHeader.vue
@@ -0,0 +1,18 @@
+<template>
+      <!-- 输入框 -->
+    <header class="header">
+      <h1>我的记事本</h1>
+      <input placeholder="请输入任务" class="new-todo" />
+      <button class="add">添加待做</button>
+    </header>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
